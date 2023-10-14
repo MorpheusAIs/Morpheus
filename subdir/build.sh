ae9452f430c8830c34721ceaef5fef9c2cab61dd
Scott Berenzweig <scott@betterbrand.com>
2023-10-14 11:36:23 -0700

    added ollama-ui as a subtree

--- subdir/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-make download_resources
-
-OUTPUT_ZIP="ollama-ui.zip"
-
-zip "$OUTPUT_ZIP" ./*.js ./*.css ./*.json ./*.html ./*.png resources/ resources/*
-
-echo "Files have been zipped as $OUTPUT_ZIP"
