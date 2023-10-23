ddc5f1433730b5932f55f2771e5230ae8b758907
Scott Berenzweig <scott@betterbrand.com>
2023-10-23 15:58:31 -0700

    push testing ollama UI

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
+  //  load the index.html of the app.
   mainWindow.loadFile('ollama-ui/index.html')
 
   // Open the DevTools.
