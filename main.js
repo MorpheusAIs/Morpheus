323368b904142fee89efa3597900edb2a8173060
Scott Berenzweig <scott@betterbrand.com>
2023-10-23 16:04:19 -0700

    just ui

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   //  load the index.html of the app.
-  mainWindow.loadFile('ollama-ui/index.html')
+  mainWindow.loadFile('ui/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
