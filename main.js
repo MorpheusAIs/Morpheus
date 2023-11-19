2abb3dc3e3be965caff210d20c3996e3effdef9b
Scott Berenzweig <scott@betterbrand.com>
2023-11-18 16:54:09 -0800

    Add Ollama process management

--- main.js
+++ main.js
@@ -40,6 +40,9 @@ app.whenReady().then(() => {
   //   // On macOS it's common to re-create a window in the app when the
   //   // dock icon is clicked and there are no other windows open.
   //   if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  runOllamaCommand(); // Serve Ollama
+  createWindow();
+
 
   // })
 })
@@ -47,9 +50,13 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+// Quit when all windows are closed, and kill Ollama process
+app.on('window-all-closed', function() {
+  if (ollamaProcess !== null) {
+      ollamaProcess.kill(); // Kill Ollama process
+  }
+  if (process.platform !== 'darwin') app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
