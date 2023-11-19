6670ceee07d32301a832fa8a28d5aa6a9ad87bf5
Scott Berenzweig <scott@betterbrand.com>
2023-11-18 16:54:09 -0800

    Add Ollama process management

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
+const { exec } = require('child_process');
+
+let ollamaProcess = null; // Variable to store the Ollama process
+
+// Define the runOllamaCommand function
+function runOllamaCommand() {
+  ollamaProcess = exec('ollama serve', (error, stdout, stderr) => {
+      if (error) {
+          console.error(`Error executing Ollama: ${error}`);
+          return;
+      }
+      console.log(`Ollama Output: ${stdout}`);
+      if (stderr) {
+          console.error(`Ollama Error Output: ${stderr}`);
+      }
+  });
+}
 
 function createWindow () {
   // Create the browser window.
@@ -23,7 +40,9 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  runOllamaCommand(); // Serve Ollama
+  createWindow();
+
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
@@ -35,9 +54,13 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+// Quit when all windows are closed, and kill Ollama process
+app.on('window-all-closed', function() {
+  if (ollamaProcess !== null) {
+      ollamaProcess.kill(); // Kill Ollama process
+  }
+  if (process.platform !== 'darwin') app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
