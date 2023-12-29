df25a700042d10dcff6ab6b9eeee79567cbd01ac
lachsbagel <lachsbagel@proton.me>
2023-12-28 23:07:29 -0600

    basic phase 1 with LCEL (hard to trace w/o langsmith acct)

--- ai_experiments/scratch.py
+++ ai_experiments/scratch.py
@@ -8,12 +8,13 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 
-# from llama_index import Document, VectorStoreIndex
-
 from contracts.contracts_loader import contracts
 from models.embedding_models import embeddings_factory
 from models.seq2seq_models import phase1_prompt_template, phase2_prompt_template
 
+
+TOP_K_METADATA = 2
+TOP_K_ABIS= 5
 
 num_contracts = len(contracts)
 documents_contracts_metadata = [str(contract[1]["metadata"]) for contract in contracts]  # preserves index order with contracts
@@ -24,16 +25,19 @@ metadata_in_memory_vectorstore = FAISS.from_texts(
     documents_contracts_metadata,
     embedding=embeddings_factory(),
 )
-metadata_retriever = metadata_in_memory_vectorstore.as_retriever()
+metadata_retriever = metadata_in_memory_vectorstore.as_retriever(search_kwargs={"k": TOP_K_METADATA})
 phase1_model = ChatOllama(model="llama2:7b")
 phase1_prompt = ChatPromptTemplate.from_template(phase1_prompt_template)
+# for debug purposes until we get langsmith access
+# docs = metadata_retriever.get_relevant_documents("I want to send 10ETH to 0xbnferkjbejknwejknvekjvnrjub")
 
 # abi_in_memory_vectorstore = FAISS.from_texts(
 #     documents_contracts_abis,
 #     embedding=embeddings_factory()
 # )
-# abi_retriever = abi_in_memory_vectorstore.as_retriever()
-
+# abi_retriever = abi_in_memory_vectorstore.as_retriever(search_kwargs={"k": TOP_K_ABIS})
+# phase2_model = ChatOllama(model="llama2:7b")
+# phase2_prompt = ChatPromptTemplate.from_template(phase2_prompt_template)
 
 if __name__ == "__main__":
 
@@ -41,7 +45,7 @@ if __name__ == "__main__":
 
     # for prompt
     # prompt_embeddings = embeddings_factory()
-    NLQ = "I want to send 10BTC to bnferkjbejknwejknvekjvnrjub"   # replace with input() or cli args
+    NLQ = "I want to send 10ETH to 0xbnferkjbejknwejknvekjvnrjub"   # replace with input() or cli args
 
     setup_and_retrieval = RunnableParallel(
         {"nlq": RunnablePassthrough(),  # gets set by the RunnableParallel.invoke() method
