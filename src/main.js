c26420a683a2753ff8a0c387baf871010b9b72cc
Scott Berenzweig <scott@betterbrand.com>
2024-02-04 15:48:16 -0800

    Add ollamaPath variable to main.js

--- src/main.js
+++ src/main.js
@@ -22,6 +22,10 @@ const {
 
 const { MetaMaskSDK, SDKProvider } = require('@metamask/sdk');
 
+const resourcesPath = process.resourcesPath;
+const ollamaPath = path.join(resourcesPath, 'app.asar.unpacked', '.webpack', 'main', 'runners', 'ollama-darwin');
+console.log(ollamaPath);
+
 // When debug is set to true, the app will log debug messages to the console
 // This will be turned on by default when running the app in non-packaged mode
 global.debug = false;
